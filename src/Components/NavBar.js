83a341527e56f2ecf187ed7b5c37ce6a2657c1aa
Sabzs <sajiyoonline@gmail.com>
2021-02-04 15:11:22 -0800

    added copyright symbol to footer

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,31 +5,31 @@ import ProfilePic from "../assets/images/sabzs-profile-pic.jpg"
 function NavBar() {
   return (
     <div className="navBar" >
-      <div className="navBar__profile-pic" >
-        <img src={ProfilePic} alt="" />
-      </div>
-      <div className="navBar__nav-menu" >
-        <ul className="navBar__nav-items" >
-          <li className="navBar__nav-list" >
+
+      <nav className="navBar__nav-menu" >
+        <div className="navBar__profile-pic" >
+          <img src={ProfilePic} alt="" />
+        </div>
+        <ul className="navBar__nav-list" >
+          <li className="navBar__nav-item" >
             <NavLink to="/" activeClassName="active" >Home</NavLink>
           </li>
-          <li className="navBar__nav-list" >
+          <li className="navBar__nav-item" >
             <NavLink to="/about" activeClassName="active" >About</NavLink>
           </li>
-          <li className="navBar__nav-list" >
+          <li className="navBar__nav-item" >
             <NavLink to="/portfolios" activeClassName="active" >Portfolios</NavLink>
           </li>
-          <li className="navBar__nav-list" >
+          <li className="navBar__nav-item" >
             <NavLink to="/blog" activeClassName="active" >Blogs</NavLink>
           </li>
-          <li className="navBar__nav-list" >
+          <li className="navBar__nav-item" >
             <NavLink to="/contact" activeClassName="active" >Contact</NavLink>
           </li>
         </ul>
-      </div>
 
-
-
+        <footer className="navBar__footer"><p> ©2021 Sabo Jise </p> </footer>
+      </nav>
 
     </div>
   )
