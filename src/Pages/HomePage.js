86b8a1e128c05b307077fc0d722c10c8e20a1fcd
Sabzs <sajiyoonline@gmail.com>
2021-02-04 18:05:19 -0800

    created and setup homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className="homePage" >
+      <div className="homePage__hero" >
+        <h1 className="homePage__hero-header" >Hi, I am <span>Sabo Jise</span></h1>
+        <p className="homePage__brief-about" >
+          I am a Full Stack Web Developer with a background in music.
+          I am passionate about the ability to create and bring imaginations to life through visual representation,
+          creating something unique that people can feel, interact and work with.
+        </p>
+      </div>
+      <div className="homePage__icons-container" >
+        <Link className="homePage__icons" ><i className="fab fa-linkedin"></i></Link>
+        <Link className="homePage__icons" ><i className="fab fa-github"></i></Link>
+      </div>
+
+
     </div>
   )
 }
