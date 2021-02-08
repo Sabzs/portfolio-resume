8badcba7df1345485ec067212109b03955d93f57
Sabzs <sajiyoonline@gmail.com>
2021-02-08 08:52:48 -0800

    added new route component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import NavBar from './Components/NavBar';
+import HomePage from './Pages/HomePage';
+import { Switch, Route, } from 'react-router-dom';
 import './Styles/App.css';
+import AboutPage from './Pages/AboutPage';
+import PortfoliosPage from './Pages/PortfoliosPage';
+
+import ContactPage from './Pages/ContactPage';
+import BlogsPage from './Pages/BlogsPage';
 
 
 function App() {
   return (
     <div className="App">
-      <div className="App__sidebar" >
-        <NavBar />
+
+      <div className="App__sidebar" ><NavBar /></div>
+
+      <div className="App__main-content" >
+        <div className="App__content" >
+          <Switch>
+            <Route path="/" exact render={() => <HomePage />} />
+            <Route path="/about" exact render={() => <AboutPage />} />
+            <Route path="/portfolios" exact render={() => <PortfoliosPage />} />
+            <Route path="/blogs" exact render={() => <BlogsPage />} />
+            <Route path="/contact" exact render={() => <ContactPage />} />
+          </Switch>
+        </div>
       </div>
-      <div className="App__main-content" ></div>
+
     </div>
   );
 }
