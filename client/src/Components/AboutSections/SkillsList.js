0e4ca3f2e6c851e770a38bb26a237c70d28644ca
Sabzs <sajiyoonline@gmail.com>
2021-04-15 22:50:41 -0700

    new SkillsList component

---
+++ client/src/Components/AboutSections/SkillsList.js
@@ -0,0 +1,38 @@
+const SkillsList = [
+  {
+    id: 1,
+    category: "Languages",
+    lang1: "CSS3 / SCSS",
+    lang2: "HTML5",
+    lang3: "Node.js",
+    lang4: "JavaScript"
+  },
+  {
+    id: 2,
+    category: "Libraries",
+    lang1: "Express",
+    lang2: "React.js",
+  },
+  {
+    id: 3,
+    category: "Tools",
+    lang1: "Git & GitHub",
+    lang2: "Chrome Dev",
+    lang3: "EsLint",
+  },
+  {
+    id: 4,
+    category: "Database",
+    lang1: "MySQL",
+    lang2: "ORM: Sequelize",
+  },
+  {
+    id: 5,
+    category: "Unit Test",
+    lang1: "Enzyyme",
+    lang2: "Jest",
+  },
+
+]
+
+export default SkillsList;
