377cf63717f0a280e8982de0cc143145f7a4fe8c
Sabzs <sajiyoonline@gmail.com>
2021-04-15 22:19:20 -0700

    modified component layout and restyled UI

--- client/src/Components/ContactList.js
+++ client/src/Components/ContactList.js
@@ -3,12 +3,10 @@ import React from 'react'
 function ContactList({ icon, text, title }) {
   return (
     <div className="contactList" >
-      <div className="contactList__contact-details">
-        <img className="contactList__item-icon" src={icon} alt="" />
-        <div className="contactList__title-text-container">
-          <h6 className="contactList__item-title" >{title}</h6>
-          <p className="contactList__item-text" >{text}</p>
-        </div>
+      <img className="contactList__item-icon" src={icon} alt="" />
+      <div className="contactList__title-text-container">
+        <h4 className="contactList__item-title" >{title}</h4>
+        <p className="contactList__item-text" >{text}</p>
       </div>
     </div>
   )
--- client/src/Pages/ContactPage.js
+++ client/src/Pages/ContactPage.js
@@ -6,6 +6,7 @@ import email from "../assets/images/email-icon.svg";
 import location from "../assets/images/location-icon.svg";
 
 function ContactPage() {
+
   return (
     <div className="contactPage" >
 
@@ -13,15 +14,18 @@ function ContactPage() {
         <Title title={"Contact Me"} span={"Contact Me"} />
       </div>
 
-
       <div className="contactPage__map-info-container" >
         <div className="contactPage__map-section">
-          <iframe title="This is a unique title" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d20853.419697131198!2d-122.68614715000001!3d49.206677899999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sca!4v1612659361663!5m2!1sen!2sca" width="600" height="450" frameBorder="0" style={{ border: 0 }} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+          <iframe title="This is a unique-section"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83300.21509383063!2d-122.74151745825907!3d49.273901229572786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5485d43ee774b3f1%3A0x953de0e394d40c00!2sPitt%20Meadows%2C%20BC!5e0!3m2!1sen!2sca!4v1618539246383!5m2!1sen!2sca"
+            loading="lazy">
+          </iframe>
         </div>
+
         <div className="contactPage__details-section">
-          <ContactList title={"Phone"} icon={phone} text={"+1 778 512 8464"} />
-          <ContactList title={"Email"} icon={email} text={"sabojise@gmail.com"} />
-          <ContactList title={"Address"} icon={location} text={"Pit Meadows"} />
+          <ContactList title={"Phone:"} icon={phone} text={"+1 778 512 8464"} />
+          <ContactList title={"Email:"} icon={email} text={"sabojise@gmail.com"} />
+          <ContactList title={"Address:"} icon={location} text={"Osprey Village, Pit Meadows"} />
         </div>
       </div>
 
