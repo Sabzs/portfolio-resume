935e4fddfa2a21841b7aeedf7b79f9b6b98b87a3
Sabzs <sajiyoonline@gmail.com>
2021-04-15 21:53:03 -0700

    added and changed navbar profile avater

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
-// import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import ProfilePic from "../assets/images/profile-navbar.jpg"
+import ProfilePic from "../assets/images/reflections.jpg"
 
 
 function NavBar({ closeMenu }) {
@@ -31,24 +30,6 @@ function NavBar({ closeMenu }) {
             <NavLink to="/contact" activeClassName="active" onClick={closeMenu}   >Contact</NavLink>
           </li>
         </ul>
-
-        {/* <ul className="navBar__nav-list" >
-          <li className="navBar__nav-item" >
-            <NavLink to="/" activeClassName="active" >Home</NavLink>
-          </li>
-          <li className="navBar__nav-item" >
-            <NavLink to="/about" activeClassName="active"   >About</NavLink>
-          </li>
-          <li className="navBar__nav-item" >
-            <NavLink to="/portfolios" activeClassName="active"  >Portfolios</NavLink>
-          </li>
-          <li className="navBar__nav-item" >
-            <NavLink to="/blogs" activeClassName="active"  >Blogs</NavLink>
-          </li>
-          <li className="navBar__nav-item" >
-            <NavLink to="/contact" activeClassName="active"   >Contact</NavLink>
-          </li>
-        </ul> */}
 
         <footer className="navBar__footer"><p> ©2021 Sabo Jise </p> </footer>
       </nav>
