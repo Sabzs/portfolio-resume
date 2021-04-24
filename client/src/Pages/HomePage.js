d13eacab7f548073a60c313126da95b69d4733bb
Sabzs <sajiyoonline@gmail.com>
2021-04-23 18:19:49 -0700

    added and styled home-title element

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,21 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import Title from "../Components/Title"
 
 function HomePage() {
   return (
     <div className="homePage" >
-      <div className="homePage__hero" >
-        <h1 className="homePage__hero-header" >Hi, I am <span>Sabo Jise</span></h1>
-        <p className="homePage__brief-about" >
-          I am a Full Stack Web Developer with a background in music.
-          I am passionate about the ability to create and bring imaginations to life through visual representation,
-          creating something unique that people can feel, interact and work with.
+
+      <div className="homePage__first-wrapper" >
+        <div className="homePage__home-title" >
+          <Title title={"Welcome"} span={"Welcome"} />
+        </div>
+      </div>
+
+      <div className="homePage__second-wrapper" >
+        <div className="homePage__hero" >
+          <h1 className="homePage__hero-header" >Hi, I am <span>Sabo Jise</span></h1>
+          <p className="homePage__brief-about" >
+            I am a Full Stack Web Developer with a background in music.
+            I am passionate about the ability to create and bring imaginations to life through visual representation,
+            creating something unique that people can feel, interact and work with.
         </p>
+        </div>
+        <div className="homePage__icons-container" >
+          <a href="https://www.linkedin.com/in/sabojise"
+            className="homePage__icons" >
+            <i className="fab fa-linkedin">
+            </i>
+          </a>
+          <a href="https://github.com/Sabzs"
+            className="homePage__icons" >
+            <i className="fab fa-github">
+            </i>
+          </a>
+        </div>
       </div>
-      <div className="homePage__icons-container" >
-        <Link to="/" className="homePage__icons" ><i className="fab fa-linkedin"></i></Link>
-        <Link to="/" className="homePage__icons" ><i className="fab fa-github"></i></Link>
-      </div>
+
+
 
 
     </div>
